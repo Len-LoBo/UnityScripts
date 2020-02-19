5de0e1ff9cd7ca9aa1bc2687c9389e4b1c34b4e4
Leonard LoBiondo <llobiondo2@gmail.com>
2020-02-18 17:09:36 -0800

    "Fixed gravity bug where player was being pushed in local down direction.  Gravity now set in global coordinate system.  Moved it beneath the TransformDirection"

--- FPSMovement.cs
+++ FPSMovement.cs
@@ -9,7 +9,6 @@ public class FPSMovement : MonoBehaviour
 {
     //movement speed
     [SerializeField] float speed = 6.0f;
-
     //gravity
     [SerializeField] float gravity = -9.8f;
 
@@ -18,24 +17,33 @@ public class FPSMovement : MonoBehaviour
 
     void Start()
     {
+        //stores character controller from gameObject
         _charController = GetComponent<CharacterController>();
-        
     }
 
     
     void Update()
     {
+        //gets movement from Horizontal and Vertical virtual mappings
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        //creates movement vector
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        //clamps magnitude of vector so diagonal movement doesn't scale up
         movement = Vector3.ClampMagnitude(movement, speed);
-        movement.y = gravity;
 
         //makes movement frame rate independent
         movement *= Time.deltaTime;
 
-
+        //changes movement from global to local coordinates
         movement = transform.TransformDirection(movement);
+
+        //keeps gravity in global coordinates
+        movement.y = gravity;
+
+        //performs movement on character controller
         _charController.Move(movement);
         
     }
